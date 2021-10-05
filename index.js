a50bae299ef5d8a576205593125e19758625f8ae
emir4ik-code <emirahmetov@gmail.com>
2021-10-05 14:31:16 +0000

    both problems solved

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 //1
-function fizzBuzz(){
-  
-}
+const fizzBuzz = () => {
+    for(let i = 1; i < 101; i++) {
+        
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log('FizzBuzz');
+        } 
+        else if(i % 3 === 0) {
+            console.log('Fizz');
+        } else if(i % 5 === 0) {
+            console.log('Buzz');
+        }else {
+            console.log(i);
+        }
+        
+        }
+    }
 
-//2
-function fizzBuzz(number){
-  
-}
+//fizzBuzz()
 
+
+const fizzBuzzNum = num => {
+    let answer = [];
+    for(let i = 1; i < num + 1; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            answer.push('FizzBuzz');
+            
+        } else if(i % 3 === 0) {
+            answer.push('Fizz');
+        } else if(i % 5 === 0) {
+            answer.push('Buzz');
+        } else {
+            answer.push(i);
+        }
+    } return answer; 
+} 
+
+//console.log(fizzBuzzNum(25));
 
